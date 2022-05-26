6b7e50804dce058aca7c8e104d34c28fe68eabfa
tetsguya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-05-26 16:16:39 +0900

    未完了の削除と完了ボタンの昨日を実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,45 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  //input.valueの初期値を設定
   const [todoText, setTodoText] = useState("");
+  //未完了のtodoリストをuseState初期化,todoへ一つずつ渡す準備
   const [incompleteTodos, setIncompleteTodos] = useState([
     "１つ目のTODO",
     "２つ目のTODO"
   ]);
+  //完了のtodoリストをuseStateで初期化
   const [completeTodos, setCompleteTodos] = useState(["完了の１つ目"]);
+  //inputの変更を検知し、アロー関数内で(event.target.value)で変更された内容をとってきて、
+  //inputのvalueをsetTodoTextで書き換える
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
+  };
+
+  //削除ボタン mapの第２引数ｄから何番目かのindexを取得する
+  const onClickDelete = (index) => {
+    //incompleteTodosの配列全てを取得
+    const newTodos = [...incompleteTodos];
+    //spliceで削除するindexと個数を指定
+    newTodos.splice(index, 1);
+    //newTodos(削除後のリスト)で更新する
+    setIncompleteTodos(newTodos);
+  };
+  //完了ボタン
+  const onClickComplete = (index) => {
+    //未完了のリスト取得
+    const newIncompleteTodos = [...incompleteTodos];
+    //押された要素を削除
+    newIncompleteTodos.splice(index, 1);
+    //完了リスト取得後、未完了から削除したindexのモノを追加
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    //以下更新
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
   };
   return (
     <>
@@ -29,12 +56,13 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {/* map関数でループを回す 、引数にはincompleteTodes（usestate）の配列が来る.*/}
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
